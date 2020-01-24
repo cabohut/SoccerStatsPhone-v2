648200875f3c6f8bf1db921efc794bbafc8c9e7c
Sam Tawfik <sam.tawfik@icloud.com>
2020-01-23 21:04:37 -0800

    Initial Commit

---
+++ SoccerStatsPhone/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SoccerStatsPhone
+//
+//  Created by sam on 1/23/20.
+//  Copyright © 2020 Cabohut. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SoccerStatsPhone/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SoccerStatsPhone
+//
+//  Created by sam on 1/23/20.
+//  Copyright © 2020 Cabohut. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
